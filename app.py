576832a495a6d2a9c45835bb1c5ed01866dbf2f2
Matthew Lin <mlin2778@gmail.com>
2022-08-04 20:33:50 -0500

    Format CSS for product display

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 from scraper.scraper import scrapeForProduct, analyzeProductData
+from PIL import Image, ImageDraw
 
 app = Flask(__name__)
 
@@ -10,12 +9,19 @@ def index():
   products = []
   if request.method == 'POST':
     try:
+      redirect('/loading')
       product_name = request.form['productInput']
       products = scrapeForProduct(product_name, 1)
       analyzeProductData(products)
+      return render_template('index.html', products=products, productName=product_name)
     except:
       return 'There was an issue scraping the data for this product from Amazon'
-  return render_template('index.html', products=products)
+  else:
+    return render_template('index.html', products=products)
+
+@app.route('/loading')
+def loading():
+  return render_template('loading.html')
 
   # image  |   name    |    price    |      numReviews      |       stars
 
