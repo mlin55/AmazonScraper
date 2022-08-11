a10fca0f1df617b02500830c31d11070e031431c
Matthew Lin <mlin2778@gmail.com>
2022-08-10 20:08:43 -0500

    add executable path

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -13,7 +13,7 @@ def scrape(productName, numProducts):
   chrome_options.add_argument("--headless")
   chrome_options.add_argument("--disable-dev-shm-usage")
   chrome_options.add_argument("--no-sandbox")
-  s = Service(os.environ.get("CHROMEDRIVER_PATH"))
+  s = Service(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
   driver = webdriver.Chrome(service=s, chrome_options=chrome_options)
 
   # go to product results page
